b42e0b534467615d050a0ce6801b996fe04a6c02
Adam MARONI <amaroni@student.42.fr>
2020-11-23 23:03:30 +0100

    Correction des problèmes de norme pour ft_strtrim.c

--- fait/ft_strtrim.c
+++ fait/ft_strtrim.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*   proto_ft_strtrim.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amaroni <amaroni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 14:18:24 by amaroni           #+#    #+#             */
-/*   Updated: 2020/11/23 15:38:09 by amaroni          ###   ########.fr       */
+/*   Updated: 2020/11/23 22:58:45 by amaroni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,46 +20,49 @@ static long	ft_strlen(char *s)
 	return (rt);
 }
 
-char	*ft_strtrim(char *s1, char *set)
+static void	loop1(int y, int *i, char *set, char *s1)
 {
-	int i;
-	int y;
-	char *array;
-	char *start;
-	char *end;
+	while (y < ft_strlen(set))
+	{
+		while (*(s1 + ++*i) == *(set + y))
+			y = 0;
+		y++;
+	}
+}
+
+static void	loop2(int y, int *i, char *set, char *s1)
+{
+	while (y < ft_strlen(set))
+	{
+		while (*(s1 + *i) == *(set + y))
+		{
+			*i -= 1;
+			y = 0;
+		}
+		y++;
+	}
+}
+
+char		*ft_strtrim(char *s1, char *set)
+{
+	int		i;
+	int		y;
+	char	*array;
+	char	*start;
+	char	*end;
 
 	i = 0;
 	y = 0;
 	array = (char*)malloc((strlen(s1) + 1) * sizeof(char));
-	while (y < ft_strlen(set))
-	{
-		while (*(s1 + i) == *(set + y))
-		{
-			i++;
-			y = 0;
-		}
-		y++;
-	}
+	loop1(y, &i, set, s1);
 	start = (s1 + i);
 	y = 0;
 	i = ft_strlen(s1) - 1;
-	while (y < ft_strlen(set))
-	{
-		while (*(s1 + i) == *(set + y))
-		{
-			i--;
-			y = 0;
-		}
-		y++;
-	}
+	loop2(y, &i, set, s1);
 	end = (s1 + i);
 	i = start - s1;
 	y = 0;
 	while (i <= (end - s1))
-	{
-		*(array + y) = *(s1 + i);
-		i++;
-		y++;
-	}
+		*(array + y++) = *(s1 + i++);
 	return (array);
 }
