60e666f5a427569be29542091d11e644f3a6f923
Adam MARONI <amaroni@student.42.fr>
2020-11-24 14:43:34 +0100

    Correction des problèmes de norme sur ft_split.c

--- fait/ft_split.c
+++ fait/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: amaroni <amaroni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 11:03:37 by amaroni           #+#    #+#             */
-/*   Updated: 2020/11/24 14:25:56 by amaroni          ###   ########.fr       */
+/*   Updated: 2020/11/24 14:42:12 by amaroni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static long	ft_strlen(char *s)
 	return (rt);
 }
 
-static int	ft_countOccurence(char *s, char c)
+static int	ft_count_occurence(char *s, char c)
 {
 	int i;
 	int y;
@@ -53,38 +53,31 @@ size_t		ft_strlcpy(char *dst, char *src, size_t size)
 	return (rt);
 }
 
-char	**ft_split(char *s, char c)
+char		**ft_split(char *s, char c)
 {
-	int i;
-	int y;
-	char **rt;
-	char *start;
-	char *scpy;
-	rt = (char**)malloc(ft_countOccurence(s,c)*sizeof(char*));
-	scpy = (char*)malloc(ft_strlen(s)*sizeof(char));
-	ft_strlcpy(scpy,s,ft_strlen(s));
+	int		i;
+	int		y;
+	char	**rt;
+	char	*start;
+	char	*scpy;
+
+	rt = (char**)malloc(ft_count_occurence(s, c) * sizeof(char*));
+	scpy = (char*)malloc(ft_strlen(s) * sizeof(char));
+	ft_strlcpy(scpy, s, ft_strlen(s));
 	i = 0;
 	while (i++ < ft_strlen(s))
-	{
 		if (*(scpy + i) == c)
 			*(scpy + i) = '\0';
-	}
 	y = 0;
 	i = 0;
 	start = scpy;
-	while ((i < ft_strlen(s)))
-	{
+	while ((i++ < ft_strlen(s)))
 		if (*(scpy + i) == '\0')
 		{
-			rt[y] = (char*)malloc((ft_strlen(start)+1)*sizeof(char));
-			ft_strlcpy(rt[y],start,ft_strlen(start)+1);
-			y++;
+			rt[y] = (char*)malloc((ft_strlen(start) + 1) * sizeof(char));
+			ft_strlcpy(rt[y++], start, ft_strlen(start) + 1);
 			start = (scpy + i) + 1;
 		}
-		i++;
-	}
-	//free(scpy);
 	rt[y] = "\0";
 	return (rt);
-	
 }
