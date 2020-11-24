2508e95cb0a59c1ab26b8cb57444ae617eae3e87
Adam MARONI <amaroni@student.42.fr>
2020-11-24 14:30:33 +0100

    ft_split.c fonctionne mais pas à la norme

---
+++ ft_split.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaroni <amaroni@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/24 11:03:37 by amaroni           #+#    #+#             */
+/*   Updated: 2020/11/24 14:25:56 by amaroni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static long	ft_strlen(char *s)
+{
+	unsigned long rt;
+
+	rt = 0;
+	while (*(s + rt) != '\0')
+		rt++;
+	return (rt);
+}
+
+static int	ft_countOccurence(char *s, char c)
+{
+	int i;
+	int y;
+
+	i = 0;
+	y = 0;
+	while (i++ < ft_strlen(s))
+	{
+		if (*(s + i) == c)
+			y++;
+	}
+	return (y);
+}
+
+size_t		ft_strlcpy(char *dst, char *src, size_t size)
+{
+	size_t y;
+	size_t rt;
+
+	y = 0;
+	rt = ft_strlen(src);
+	while (y < size - 1)
+	{
+		*(dst + y) = *(src + y);
+		y++;
+	}
+	*(dst + y) = '\0';
+	return (rt);
+}
+
+char	**ft_split(char *s, char c)
+{
+	int i;
+	int y;
+	char **rt;
+	char *start;
+	char *scpy;
+	rt = (char**)malloc(ft_countOccurence(s,c)*sizeof(char*));
+	scpy = (char*)malloc(ft_strlen(s)*sizeof(char));
+	ft_strlcpy(scpy,s,ft_strlen(s));
+	i = 0;
+	while (i++ < ft_strlen(s))
+	{
+		if (*(scpy + i) == c)
+			*(scpy + i) = '\0';
+	}
+	y = 0;
+	i = 0;
+	start = scpy;
+	while ((i < ft_strlen(s)))
+	{
+		if (*(scpy + i) == '\0')
+		{
+			rt[y] = (char*)malloc((ft_strlen(start)+1)*sizeof(char));
+			ft_strlcpy(rt[y],start,ft_strlen(start)+1);
+			y++;
+			start = (scpy + i) + 1;
+		}
+		i++;
+	}
+	//free(scpy);
+	rt[y] = "\0";
+	return (rt);
+	
+}
