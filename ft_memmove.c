c8b54e78ee2f7a17be8fcb52ed9010f72dc85659
Adam MARONI <amaroni@e2r11p17.42.fr>
2020-12-16 23:38:41 +0100

    correction de memmove pour les test

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: amaroni <amaroni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 16:19:48 by amaroni           #+#    #+#             */
-/*   Updated: 2020/12/15 08:56:09 by amaroni          ###   ########.fr       */
+/*   Updated: 2020/12/16 23:24:52 by amaroni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	*ft_memmove(void *dest, void *src, size_t n)
 	d = dest;
 	lastd = d + (n - 1);
 	lasts = s + (n - 1);
+	if (!src && !dest)
+		return (dest);
 	if (d < s)
 		while (n--)
 			*d++ = *s++;
